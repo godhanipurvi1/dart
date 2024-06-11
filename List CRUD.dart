4b23004e643e2cb63c80e679aa93dbe210f2280c
godhanipurvi1 <167908469+godhanipurvi1@users.noreply.github.com>
2024-06-12 00:44:48 +0530

    Add files via upload

--- List CRUD.dart
+++ List CRUD.dart
@@ -2,64 +2,69 @@ import 'dart:io';
 List<int> l=[];
 void create()
 {
-  print("enter the numer of elemnt:");
+  print("enter the size:");
   int n=int.parse(stdin.readLineSync()??"0");
   for(int i=0;i<n;i++)
   {
-    print('Enter a number:');
+    print('Enter a number ${i+1}:');
         int number = int.parse(stdin.readLineSync()!);
         l.add(number);
-
   }
   print("$l");
 }
+
 void read()
 {
   print("List: $l");
 }
-void update()
+
+int update(int updateind ,int updateele)
 {
-print("enter a number of the index to update:");
-  int updateind=int.parse(stdin.readLineSync()??"0");
-
-  print("enter a number of the element to update:");
-  int updateele=int.parse(stdin.readLineSync()??"0");
-
   l[updateind]=updateele;
+  return updateele;
 }
-void delete()
-{
-  print("enter a number of the index to delete:");
-  int deleteind=int.parse(stdin.readLineSync()??"0");
-  l.removeAt(deleteind);
-  }
+int delete(int deleteind) => l.removeAt(deleteind);
+
 void main(){
-    int choice=1;
+    int choice=0;
     
     do{
-      print("enter 1 for CREAT\nenter 2 for READ\nenter 3 for UPDATE\nenter 4 for DELETE\nenter 5 for EXIT");
+      print("1 CREAT\n2 READ\n3 UPDATE\n4 DELETE\n5 EXIT");
       int choice=int.parse(stdin.readLineSync()??"0");
 
       switch(choice){
         case 1:
         create();
         break;
+
         case 2:
         read();
         break;
+        
         case 3:
-        update()  ;
+          print("enter a number of the index to update:");
+          int updateind=int.parse(stdin.readLineSync()??"0");
+
+          print("enter a number of the element to update:");
+          int updateele=int.parse(stdin.readLineSync()??"0");
+
+          int r=update(updateind ,updateele);
+
         break;
         case 4:
-        delete();
+          print("enter a number of the index to delete:");
+          int dind=int.parse(stdin.readLineSync()??"0");
+
+          int r1=delete(dind);
+
         break;
+
         case 5:
         print("exiting....");
         break;
+
         default:
         print("invalid choice");
-        
       }
-    } while(choice !=5);
-
+    }while(choice != 5);
 }
